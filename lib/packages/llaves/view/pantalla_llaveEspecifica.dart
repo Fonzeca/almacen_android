f600a6398de4e19c215ed7b65acf5071d7f7d324
Daiko <daiko_011@hotmail.com>
2021-06-27 12:04:01 -0300

    validation of selected user & only showing the corresponding E/S button

--- lib/packages/llaves/view/pantalla_llaveEspecifica.dart
+++ lib/packages/llaves/view/pantalla_llaveEspecifica.dart
@@ -19,111 +19,106 @@ class LlaveEspecifica extends StatelessWidget{
     return BlocListener<LlaveBloc, LlaveState>(listener: (context, state){
       if(state.carga){
         EasyLoading.show();
-    }else{
+      }else{
         EasyLoading.dismiss();
       }
     },
-    child: BlocBuilder<LlaveBloc,LlaveState>(
-      builder: (context,state){
-        if(state.llave == null){
-          return Container(child: Text("No se encontró llave con el id ingresado, por favor inténtelo nuevamente."),);
-        }else{
-          llave = state.llave;
-          return Container(
-            height: double.infinity,
-            child: SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.all(25.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
+      child: BlocBuilder<LlaveBloc,LlaveState>(
+        builder: (context,state){
+          if(state.llave == null){
+            return Container(child: Text("No se encontró llave con el id ingresado, por favor inténtelo nuevamente."),);
+          }else{
+            llave = state.llave;
+            return Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.all(25.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
 
-                  children: <Widget>[
-                    Row(
-                      children: [
-                        Expanded(child:
-                        Text("Número de copia",style: TextStyle(color: Colors.grey),),
-                        ),
-                        Expanded(child:
-                        Text("Nombre",style: TextStyle(color: Colors.grey),),
-                        )
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child:
-                        Text(llave.copia),
-                        ),
-                        Expanded(
-                          child:
-                          Text(llave.nombre),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 20.0,),
-                    Row(
-                      children: [
-                        Expanded(child: Text("Ubicación",style: TextStyle(color: Colors.grey),)),
-                        Expanded(child: Text("Observaciones", style: TextStyle(color: Colors.grey),))
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Expanded(child: Text(llave.ubicacion)),
-                        Expanded(child: Text(llave.observaciones),)
-                      ],
-                    ),
-                    SizedBox(height: 20.0,),
-                    Center(
-                      child: Text("Estado",style: TextStyle(color:Colors.grey, fontSize: 16),),
-                    ),
-                    Center(
-                      child: Text(llave.estado,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
-                    ),
-                    SizedBox(height: 20.0,),
-                    _buildButtons(context),
-                  ],
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          Expanded(child:
+                          Text("Número de copia",style: TextStyle(color: Colors.grey),),
+                          ),
+                          Expanded(child:
+                          Text("Nombre",style: TextStyle(color: Colors.grey),),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(child:
+                          Text(llave.copia),
+                          ),
+                          Expanded(
+                            child:
+                            Text(llave.nombre),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 20.0,),
+                      Row(
+                        children: [
+                          Expanded(child: Text("Ubicación",style: TextStyle(color: Colors.grey),)),
+                          Expanded(child: Text("Observaciones", style: TextStyle(color: Colors.grey),))
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Expanded(child: Text(llave.ubicacion)),
+                          Expanded(child: Text(llave.observaciones),)
+                        ],
+                      ),
+                      SizedBox(height: 20.0,),
+                      Center(
+                        child: Text("Estado",style: TextStyle(color:Colors.grey, fontSize: 16),),
+                      ),
+                      Center(
+                        child: Text(llave.estado,style: TextStyle(fontSize: 18,fontWeight: FontWeight.bold),),
+                      ),
+                      SizedBox(height: 20.0,),
+                      _buildButtons(context,llave.estado=="En uso"),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
+            );
 
-        }
-      },
-    ),);
+          }
+        },
+      ),);
 
 
   }
 
 
-  Widget _buildButtons(BuildContext context) {
+  Widget _buildButtons(BuildContext context, bool entrada) {
     return Container(
 
       padding: EdgeInsets.symmetric(vertical: 15.0),
       width: double.infinity,
-      child: Row(
-        children: [
-          Expanded(
-            child: ElevatedButton(
-                style:
-                ElevatedButton.styleFrom(primary:Colors.green),
-                onPressed: ()=> registrarEntrada(context),
-                child: Text("E",style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),)),
-          ),
-          SizedBox(width:20.0),
-          Expanded(
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(primary:Colors.red),
-                onPressed: ()=> registrarSalida(context),
-                child: Text("S", style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18.0,
-                ),)),
-          )
-        ],
-      ),
+      child:entrada?
+      ElevatedButton(
+            style:
+            ElevatedButton.styleFrom(primary:Colors.green),
+            onPressed: ()=> registrarEntrada(context),
+            child: Text("Entrada",style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),)
+      ):
+      ElevatedButton(
+            style: ElevatedButton.styleFrom(primary:Colors.red),
+            onPressed: ()=> registrarSalida(context),
+            child: Text("Salida", style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 18.0,
+            ),)
+      )
+      ,
     );
   }
 
@@ -179,14 +174,18 @@ class LlaveEspecifica extends StatelessWidget{
                   ),
                 ),
                 TextButton(onPressed: () {
-                  BlocProvider.of<LlaveBloc>(context).add(LlaveCambiarEstado(id,"En uso",username));
-                  Navigator.pop(context);
+                  if(username != null){
+                    BlocProvider.of<LlaveBloc>(context).add(LlaveCambiarEstado(id,"En uso",username));
+                    Navigator.pop(context);
+                  }else{
+                    EasyLoading.showToast("Debe seleccionar un usuario");
+                  }
 
                 }, child: Text("OK")),
               ],
             ) ,
           );
-  });
+        });
   }
 
   registrarEntrada(BuildContext context) {
